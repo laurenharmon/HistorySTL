e0c65ef8973bd7f4786d3d4e0fc61878ab83f0a9
Lauren Harmon <harmon.lauren@hotmail.com>
2021-08-03 15:03:43 -0500

    what is even happening!!!

--- models/site.js
+++ models/site.js
@@ -34,20 +34,54 @@ SiteSchema.virtual("properties.popUpInfo").get(function () {
 
 SiteSchema.virtual("properties.clickDisplay").get(function () {
     const response = `
-    <article class="history-card">
-    <div class="content">
-    <div class="card">
-    <div class="card-header">
-    <div class="card-header-title"><p>${this.name}</p></div></div>
-    <div class="card-content">
-      <div class="content facts">
-        ${this.history}
-        <br>
-        <p><strong>Developed In:</strong> ${this.yearDeveloped}</p>
-      </div>
-      </div>
-      
-  </div></article></div>`;
+        <div class="history-card" onload="init()">
+            <div class="content">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="card-header-title"><p class="is-pulled-left">${this.name}</p></div>
+                        <div class="tabs is-pulled-right mr-3">
+                        <ul id="tabs">
+                            <li class="history-tab">
+                                <a href="#history">History</a>
+                            </li>
+                            <li class="year-tab">
+                                <a href="#year">Year Developed</a>
+                            </li>
+                            <li class="filler-tab">
+                                <a href="#filler">Filler</a>
+                            </li>
+                        </ul>
+                    </div>
+                        </div>
+                            <div class="card-content history">
+                                <div class="content">
+                                    <div class="tabContent" id="history">
+                                        <div>
+                                          <p>${this.history}</p>
+                                        </div>
+                                      </div>
+
+                                      <div class="tabContent" id="year">
+                                      <div>
+                                        <p>${this.yearDeveloped}</p>
+                                      </div>
+                                    </div>
+
+                                    <div class="tabContent" id="filler">
+                                    <div>
+                                      <p>FILLER STUFF HERE</p>
+                                    </div>
+                                  </div>
+
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`;
     return response;
 });
 
--- public/scripts/bulma.js
+++ public/scripts/bulma.js
@@ -6,3 +6,64 @@
         menu.classList.toggle('is-active');
     });
 })();
+
+function init() {
+
+    // Grab the tab links and content divs from the page
+    let tabListItems = document.getElementById('tabs').childNodes;
+    for ( let i = 0; i < tabListItems.length; i++ ) {
+      if ( tabListItems[i].nodeName == "LI" ) {
+        let tabLink = getChildWithTagName( tabListItems[i], 'A' );
+        let id = getHash( tabLink.getAttribute('href') );
+        tabLinks[id] = tabLink;
+        contentDivs[id] = document.getElementById( id );
+      }
+    }
+
+    // Assign onclick events to the tab links, and
+    // highlight the first tab
+    let i = 0;
+
+    for ( let id in tabLinks ) {
+      tabLinks[id].onclick = showTab;
+      if ( i == 0 ) {
+          tabLinks[id].className = "tabContent selected";
+      } else {
+          contentDivs[id].className = "tabContent hide"
+      }
+      i++;
+    }
+  }
+
+
+  function showTab() {
+    let selectedId = getHash( this.getAttribute('href') );
+
+    // Highlight the selected tab, and dim all others.
+    // Also show the selected content div, and hide all others.
+    for ( let id in contentDivs ) {
+      if ( id == selectedId ) {
+        tabLinks[id].className = 'selected';
+        contentDivs[id].className = 'tabContent';
+      } else {
+        tabLinks[id].className = '';
+        contentDivs[id].className = 'tabContent hide';
+      }
+    }
+
+    // Stop the browser following the link
+    return false;
+  }
+
+
+  function getFirstChildWithTagName( element, tagName ) {
+    for ( let i = 0; i < element.childNodes.length; i++ ) {
+      if ( element.childNodes[i].nodeName == tagName ) return element.childNodes[i];
+    }
+  }
+
+  function getHash( url ) {
+    let hashPos = url.lastIndexOf ( '#' );
+    return url.substring( hashPos + 1 );
+  }
+
